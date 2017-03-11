e6ac0d7a3154af5b0837d3116ccf112aad4a2fae
Tuesday Account <TEMPtue@ACADEMIC.CAMPUS.NJIT.EDU>
2017-03-11 12:45:37 -0500

    follower complete

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -2,16 +2,36 @@
 using System.Collections;
 
 public class Follower : MonoBehaviour {
-	public Transform startmarker;
+	
 	public Transform endMarker;
 	public float speed = 1.0f;
+	public Vector3 offset;
+	public float checkRate;
+	public float nextCheck;
+
+	public PlayerController followedChar;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () {	
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Debug.Log ("Time: "+Time.time);
+		//Debug.Log ("Next Tick: "+nextTick.ToString());
+		if (Time.time > nextCheck) {			
+			nextCheck = Time.time + checkRate;
+			if (followedChar.horizontal == 0)
+				checkRate = Random.Range (3f, 5f);
+			else
+				checkRate = Random.Range (7f, 11f);
+			RandomizeOffset ();
+		}
+		transform.position = Vector2.Lerp (transform.position, endMarker.position + offset, speed*Time.deltaTime);
+	}
+	void RandomizeOffset(){
+		offset.x = Random.Range(-2f,1f);
+		offset.y = Random.Range(0f,1.5f);
 	}
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,44 +9,54 @@ public class PlayerController : MonoBehaviour
 	public float jumpForce;
 
 	public bool isGrounded;
+	public bool isJumpEmpowered;
+	public float horizontal;
 	public float speed;
 
 
 	// Use this for initialization
 	void Start () {
 		myRigidbody= GetComponent<Rigidbody2D> ();
-		speed = 5f;
-		jumpDir =new Vector2 (0, 1);
-		jumpForce = 5;
+		speed = 10f;
+		jumpDir = new Vector2 (0, 1);
+		jumpForce = 6f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GetInput ();
+
 	}
 	void FixedUpdate(){
 	}
 	void GetInput(){
-		transform.Translate (new Vector2 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0));
-		if (isGrounded) 
-		{
-			if(Input.GetKeyDown(KeyCode.Space))
-			{
-				myRigidbody.AddForce(jumpDir*jumpForce,ForceMode2D.Impulse);
+
+		if (isGrounded) {
+			horizontal = Input.GetAxis ("LeftJoystickHorizontal");
+			if (horizontal != 0) {
+				transform.Translate (horizontal * Vector2.right * speed * Time.deltaTime);
 			}
-		}	
+			if (Input.GetButtonDown ("A")) {
+				Debug.Log ("oi");
+				myRigidbody.AddForce (jumpDir * jumpForce, ForceMode2D.Impulse);
+			}
+		} else {
+			transform.Translate (horizontal * Vector2.right * speed * Time.deltaTime);
+		}
+
 	}
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag == "ground") {
 			speed = 5f;
 			isGrounded = true;
+			isJumpEmpowered = false;
 		}
 	}
 	void OnCollisionExit2D(Collision2D other)
 	{
 		if (other.gameObject.tag == "ground") {
-			speed = .5f;
+			speed = 5f;
 			isGrounded = false;
 		}
 	}
