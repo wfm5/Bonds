980dba649bd526d6ee610c75abf54ee6ee210f0f
Tuesday Account <TEMPtue@ACADEMIC.CAMPUS.NJIT.EDU>
2017-03-10 23:57:17 -0500

    BTW GJ 1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public Rigidbody2D myRigidbody;
+
+	public Vector2 jumpDir;
+	public float jumpForce;
+
+	public bool isGrounded;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody= GetComponent<Rigidbody2D> ();
+		speed = 5f;
+		jumpDir =new Vector2 (0, 1);
+		jumpForce = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput ();
+	}
+	void FixedUpdate(){
+	}
+	void GetInput(){
+		transform.Translate (new Vector2 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0));
+		if (isGrounded) 
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				myRigidbody.AddForce(jumpDir*jumpForce,ForceMode2D.Impulse);
+			}
+		}	
+	}
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "ground") {
+			speed = 5f;
+			isGrounded = true;
+		}
+	}
+	void OnCollisionExit2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "ground") {
+			speed = .5f;
+			isGrounded = false;
+		}
+	}
+}
