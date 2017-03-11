1b99905a15550815007f0fb0e6504b192ad2d491
Tuesday Account <TEMPtue@ACADEMIC.CAMPUS.NJIT.EDU>
2017-03-11 01:20:39 -0500

    Think this works, untested

--- Assets/OnTrigger.cs
+++ Assets/OnTrigger.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using System.IO;
 
 public class OnTrigger : MonoBehaviour {
 
     EventTrigger trigger;
+    public Globals glob;
     public string FileName;
     bool Started = false;
     string buffer = "";
@@ -20,7 +22,16 @@ public class OnTrigger : MonoBehaviour {
 
     void PopulateEventSystem(string filename)
     {
-
+        string[] str_lst = File.ReadAllLines(filename);
+        for (int i = 0; i < str_lst.Length; i++)
+        {
+            if (str_lst[i].Length > 0)
+            {
+                EventTrigger.Entry entry = new EventTrigger.Entry();
+                entry.eventID = EventTriggerType.Submit;
+                entry.callback.AddListener((data) => { glob.AddToQ(str_lst[i]); } );
+            }
+        }
     }
 
     void OnTriggerEnter(Collider col)
