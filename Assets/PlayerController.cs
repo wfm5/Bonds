79f0d5c4cb0c8e6c4dddbd139d4fd24d9b2d3ec9
Branden <br66@njit,edu>
2017-03-11 21:26:51 -0500

    jank ledge grip in beta

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,7 +15,8 @@ public class PlayerController : MonoBehaviour
 
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		myRigidbody= GetComponent<Rigidbody2D> ();
 		speed = 10f;
 		jumpDir = new Vector2 (0, 1);
@@ -23,41 +24,67 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		GetInput ();
-
 	}
-	void FixedUpdate(){
+	void FixedUpdate()
+	{
+		Debug.Log (GetComponent<Rigidbody2D> ().velocity);
 	}
-	void GetInput(){
+	void GetInput()
+	{
 
-		if (isGrounded) {
-			horizontal = Input.GetAxis ("LeftJoystickHorizontal");
-			if (horizontal != 0) {
+		if (isGrounded) 
+		{
+			horizontal = Input.GetAxis ("Horizontal");
+			if (horizontal != 0) 
+			{
 				transform.Translate (horizontal * Vector2.right * speed * Time.deltaTime);
 			}
-			if (Input.GetButtonDown ("A")) {
+			if (Input.GetKeyDown(KeyCode.Space)) 
+			{
+				GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.None;
 				Debug.Log ("oi");
 				myRigidbody.AddForce (jumpDir * jumpForce, ForceMode2D.Impulse);
 			}
-		} else {
+		} 
+		else 
+		{
 			transform.Translate (horizontal * Vector2.right * speed * Time.deltaTime);
 		}
 
 	}
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (other.gameObject.tag == "ground") {
+		if (other.gameObject.tag == "ground") 
+		{
 			speed = 5f;
 			isGrounded = true;
 			isJumpEmpowered = false;
 		}
+		if(other.gameObject.tag == "enemy")
+		{
+			Debug.Log ("enemy");
+			myRigidbody.AddForceAtPosition (new Vector2 ((other.gameObject.transform.position.x - transform.position.x)*3, 1f), transform.position);
+		}
 	}
 	void OnCollisionExit2D(Collision2D other)
 	{
-		if (other.gameObject.tag == "ground") {
+		if (other.gameObject.tag == "ground") 
+		{
 			speed = 5f;
 			isGrounded = false;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "ledge") 
+		{
+			speed = 0;
+			isGrounded = true;
+			GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePosition;
 		}
 	}
 }
